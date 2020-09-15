053b3c869bfb74374ba068493f50cea7060c356b
murilinhoPs <murilinhops@hotmail.com>
2020-09-15 12:47:41 -0300

    adding "create choice" function

--- src/components/choices/index.js
+++ src/components/choices/index.js
@@ -1,3 +1,17 @@
-function CreateChoiceField() {
-  console.log('Will create another choice');
+function CreateNewChoice(newFieldId, insertPos) {
+  let newFields = document.getElementsByClassName(newFieldId);
+
+  //.cloneNode(true);
+  let inserts_here = document.getElementsByClassName(insertPos);
+
+  for (let newField of newFields) {
+    // btnElm.addEventListener('click', RemoveElm);
+  }
+
+  console.log(newFields);
+  console.log(inserts_here);
+
+  // newField.style.display = 'block';
+
+  // insert_here.parentNode.insertBefore(newField, insert_here);
 }
--- src/components/forms/index.js
+++ src/components/forms/index.js
@@ -17,22 +17,40 @@ function getData() {
   // }
 }
 
-let counter = 0;
+let narrativeCounter = 0;
+let choiceCounter = 0;
 
-function CreateNarrativeField() {
-  counter++;
+function CreateNewCard(newFieldId, insertPos) {
+  narrativeCounter++;
+  // else choiceCounter++;
 
-  var newField = document
-    .getElementById('json_form_read')
-    .cloneNode(true);
+  var newField = document.getElementById(newFieldId).cloneNode(true);
+  var insert_here = document.getElementById(insertPos);
+
+  RenameNodes(newField);
 
   newField.style.display = 'block';
 
-  var insert_here = document.getElementById('insert_here');
   insert_here.parentNode.insertBefore(newField, insert_here);
 
   collapse();
   updateButton();
 }
 
-window.onload = CreateNarrativeField;
+function RenameNodes(newFields) {
+  for (let field of newFields.childNodes) {
+    var theName = field.id;
+
+    if (theName !== undefined) {
+      field.id = theName + narrativeCounter;
+    }
+
+    if (field.id === 'collapsible_content_' + narrativeCounter) {
+      console.log(field); //[5].childNodes);
+
+      console.log(field.childNodes[5].childNodes);
+    }
+  }
+}
+
+window.onload = CreateNewCard('json_form_read', 'insert_here');
